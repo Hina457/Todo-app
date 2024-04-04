79aaaa69bd11e2087cab15d493a34068db795c53
Hina Alvi <princess4177812@gmail.com>
2024-04-04 13:10:58 -0700

    push on my  github

---
+++ todo.ts
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+let todos =[];
+let condition = true;
+
+while(condition){
+let addtask = await inquirer.prompt(
+    [
+        {
+            name:"todo",
+            type:"input",
+            message:"What you want to add in your todos?",
+        },
+        {
+            name:"addmore",
+            type:"confirm",
+            message:"Do you want to add more?",
+            default:"false",
+
+        }
+    ]
+);
+
+let optionAns =await inquirer.prompt(
+    [
+        {
+       name:"Delete",
+       type:"list",
+       message:"Which action do you want to perform?",
+       choices:["add","Edit","Delete"]
+        }
+    ]
+);
+console.log(optionAns);
+
+todos.push(addtask.todo)
+condition =addtask .addmore
+console.log(todos)
+}
